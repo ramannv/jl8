b9160a0f37871d2f97bf8dfa675dfd0099c77e9f
Raman Nv <rnvnoc@gmail.com>
2017-05-22 19:21:42 +0530

    Basic features: - swipe left to go to next image - swipe right to go to previous image

---
+++ app/src/main/java/com/jl8/ramannv/jl8/MainActivity.java
@@ -0,0 +1,120 @@
+package com.jl8.ramannv.jl8;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+import com.bumptech.glide.*;
+
+class OnSwipeTouchListener implements OnTouchListener {
+
+    private final GestureDetector gestureDetector;
+
+    public OnSwipeTouchListener (Context ctx){
+        gestureDetector = new GestureDetector(ctx, new GestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    private final class GestureListener extends SimpleOnGestureListener {
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                        result = true;
+                    }
+                }
+                else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom();
+                    } else {
+                        onSwipeTop();
+                    }
+                    result = true;
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+    }
+
+    public void onSwipeRight() {
+    }
+
+    public void onSwipeLeft() {
+    }
+
+    public void onSwipeTop() {
+    }
+
+    public void onSwipeBottom() {
+    }
+}
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageview;
+    private static int i = 1;
+    ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String imlink = "http://limbero.org/jl8/comics/" + String.valueOf(i) + ".jpeg";
+        imageview = (ImageView) findViewById(R.id.imageView);
+        final Intent in = new Intent(this.getApplicationContext(), this.getClass());
+        Glide.with(this).load(imlink).asBitmap().placeholder(R.drawable.loading).error(R.drawable.four).fitCenter().into(imageview);
+        imageview.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+
+
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "next", Toast.LENGTH_SHORT).show();
+                i++;
+                startActivity(in);
+            }
+
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "prev", Toast.LENGTH_SHORT).show();
+                i--;
+                startActivity(in);
+
+            }
+
+        });
+    }
+}
